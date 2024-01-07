e9b8b3f97aedada5815cd47bb9c48889ed59a20d
Vinzel <75076070+vinzel-ops@users.noreply.github.com>
2024-01-08 01:11:44 +0700

    adds

---
+++ interact/includes/vars.sh
@@ -0,0 +1,24 @@
+names=("aspen" "austin" "bango" "banzai" "bartik" "bassi" "batman" "beaver" "bell" "benz" "blink" "borg" "bose" "boyd" "buck" "cage" "cannon" "cass" "cerf" "chell" "clarke" "cohen" "colden" "cori" "cray" "darwin" "dawgyg" "diffie" "dirac" "elion" "ellis" "euler" "fermi" "fire" "fisher" "fox" "gates" "gauss" "ghost" "gould" "haddix" "haibt" "hertz" "hickey" "hoover" "hopper" "hugle" "hunt" "jang" "jarvis" "jepsen" "jobs" "joliot" "jones" "kalam" "kare" "keller" "kepler" "kilby" "kirch" "knox" "knuth" "lamar" "lamp" "lande" "leaky" "leder" "leman" "lewin" "liskov" "loka" "luke" "lupin" "martho" "mato" "max" "mayer" "mclean" "medin" "mendel" "merkle" "mog" "moore" "morse" "moser" "murdo" "nagli" "napier" "nash" "nat" "neum" "newton" "night" "nobel" "noyce" "octavian" "ofjaaah" "omnom" "pani" "pare" "pasa" "payne" "pdelteil" "perl" "pikpikcu" "poba" "pry" "raman" "rez" "rhodes" "rich" "ride" "robin" "rubin" "saha" "sammet" "securibee" "sandeep" "snyder" "stok" "stone" "suther" "swartz" "tesla" "thl" "thomp" "todayisnew" "tu" "turing" "victoni" "vince" "wright" "wu" "xpn")
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+export Black='\033[0;30m'        # Black
+export Red='\033[0;31m'          # Red
+export Green='\033[0;32m'        # Green
+export Yellow='\033[0;33m'       # Yellow
+export Blue='\033[0;34m'         # Blue
+export Purple='\033[0;35m'       # Purple
+export Cyan='\033[0;36m'         # Cyan
+export White='\033[0;37m'        # White
+
+# Bold
+export BBlack='\033[1;30m'       # Black
+export BRed='\033[1;31m'         # Red
+export BGreen='\033[1;32m'       # Green
+export BYellow='\033[1;33m'      # Yellow
+export BBlue='\033[1;34m'        # Blue
+export BPurple='\033[1;35m'      # Purple
+export BCyan='\033[1;36m'        # Cyan
+export BWhite='\033[1;37m'       # White
